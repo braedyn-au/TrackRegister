d47bbd3f8237fc313292f692dfc857878710e7e9
Braedyn-Au <braedyn.au@gmail.com>
2019-10-09 11:53:44 -0600

    updated parseCSV

--- parseCSV.m
+++ parseCSV.m
@@ -4,32 +4,37 @@ function trackPoints = parseCSV(csvfile,conversion)
 
 data = readmatrix(csvfile);
 trackPoints = {};
-start = 2;
+start = 2; %skip the header of CSV file
 for row = 2:(size(data,1)-1)
     xpoints = [];
     ypoints = [];
     zpoints = [];
     tpoints = [];
-    if data(row,2) ~= data(row+1,2) 
-        xpoints = [xpoints,data(start:row,4)].'./conversion;
-        ypoints = [ypoints,data(start:row,5)].'./conversion;
-        zpoints = [zpoints,data(start:row,6)].'./conversion;
-        tpoints = [tpoints,data(start:row,3)].';
-        trackPoints(data(row,2),:) = {[xpoints;ypoints;zpoints;tpoints]}; 
-        start = row + 1;
+    % parse the trajectory column to keep different particles seperate
+    % once trajectory number changes, add the localizations to the array
+    % data(row,n) where n is the column of trajectories
+    % IF CHANGING THIS MAKE SURE TO CHANGE THE BOTTOM LINE TOO
+    if data(row,1) ~= data(row+1,1) 
+        xpoints = [xpoints,data(start:row,3)].'./conversion; % x column
+        ypoints = [ypoints,data(start:row,4)].'./conversion; % y column
+        zpoints = [zpoints,data(start:row,5)].'./conversion; % z column
+        tpoints = [tpoints,data(start:row,2)].'; % frames column
+        trackPoints(data(row,1),:) = {[xpoints;ypoints;zpoints;tpoints]}; 
+        start = row + 1; % update starting point for next trajectory
     end
 end
+% LAST TRACK
 % Get the last track
 xpoints = [];
 ypoints = [];
 zpoints = [];
 tpoints = [];
-
-xpoints = [xpoints,data(start:size(data,1),4)].'./conversion;
-ypoints = [ypoints,data(start:size(data,1),5)].'./conversion;
-zpoints = [zpoints,data(start:size(data,1),6)].'./conversion;
-tpoints = [tpoints,data(start:size(data,1),3)].';
-
-trackPoints(data(start,2),:) = {[xpoints;ypoints;zpoints;tpoints]};
+% UPDATE THESE COLUMN POINTS
+xpoints = [xpoints,data(start:size(data,1),3)].'./conversion;
+ypoints = [ypoints,data(start:size(data,1),4)].'./conversion;
+zpoints = [zpoints,data(start:size(data,1),5)].'./conversion;
+tpoints = [tpoints,data(start:size(data,1),2)].';
+% UPDATE
+trackPoints(data(start,1),:) = {[xpoints;ypoints;zpoints;tpoints]};
 end
 
